dae373cc3b1d3b9696081388864fc4a51d16fd41
negabaro kim <negabaro@naver.com>
2018-11-14 18:10:00 +0000

    15. Turning seconds into minutes

--- components/Timer/presenter.js
+++ components/Timer/presenter.js
@@ -2,10 +2,43 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet, StatusBar } from 'react-native';
 import Button from '../Button';
 
+function formatTime(time) {
+   var minutes = Math.floor(time / 60);
+   time -= minutes * 60;
+
+   var seconds = parseInt(time % 60, 10);
+
+   return `${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10
+     ? `0${seconds}`
+     : seconds}`;
+
+   return;
+ }
+
 class Timer extends Component {
+    componentWillReceiveProps(nextProps){
+        const currentProps = this.props;
+        console.log("currentProps: ",currentProps);
+        console.log("nextProps: ",nextProps);
+        
+        if (!currentProps.isPlaying && nextProps.isPlaying){
+            //현재 isPlaying=false이고 새로운 이벤트로 인해 isPlaying=true일 경우 타이머 시작
+            const timerInterval = setInterval(() => {
+                currentProps.addSecond();
+            }, 1000);
+            
+            this.setState({
+                interval: timerInterval
+            });
+        } else if (currentProps.isPlaying && !nextProps.isPlaying) {
+            //현재 isPlaying=true이고 새로운 이벤트가 isPlaying=false일 경우 타이머 중지
+            clearInterval(this.state.interval);
+        }
+    }
     render(){
         const { isPlaying, elapsedTime, timerDuration, restartTimer22, startTimer22, stopTimer22  } = this.props;
         console.log('Timer this.props:');
+        console.log(elapsedTime);
         console.log(this.props);
         console.log(isPlaying);
         return(
@@ -13,7 +46,7 @@ class Timer extends Component {
             <StatusBar barStyle={"light-content"} />
             <View style={styles.upper}>
               <Text style={styles.time}>
-                25:00
+                { formatTime(timerDuration - elapsedTime) }
               </Text>
             </View>
             <View style={styles.lower}>
@@ -22,7 +55,8 @@ class Timer extends Component {
               {/* isPlaying ? {} : null */}
               {/* isPlaying && {}  */}
               { !isPlaying &&<Button iconName="play-circle" onPress={ startTimer22 } /> }
-              { isPlaying &&<Button iconName="stop-circle" onPress={ stopTimer22 } /> }
+              { isPlaying &&<Button iconName="stop-circle" onPress={ restartTimer22 } /> }
+             
             </View>
           </View>
         )
